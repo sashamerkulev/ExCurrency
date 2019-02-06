5ac31423407aa6786f255b1bbcabd8a5c2823ae7
sasha merkulev <a_lexandr@mail.ru>
2019-02-06 17:25:39 +0300

    have adding currency converter usecase tests

--- app/src/test/java/ru/merkulyevsasha/excurrency/domain/calculator/CurrencyCalculatorImplTest.java
+++ app/src/test/java/ru/merkulyevsasha/excurrency/domain/calculator/CurrencyCalculatorImplTest.java
@@ -2,7 +2,10 @@ package ru.merkulyevsasha.excurrency.domain.calculator;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class CurrencyCalculatorImplTest {
 
     private CurrencyCalculator calculator = new CurrencyCalculatorImpl();
---
+++ app/src/test/java/ru/merkulyevsasha/excurrency/domain/usecase/CurrencyConverterUsecaseTest.java
@@ -0,0 +1,13 @@
+package ru.merkulyevsasha.excurrency.domain.usecase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class CurrencyConverterUsecaseTest {
+
+    @Test
+    public void convert() {
+    }
+}
